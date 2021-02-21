963dd4f34e6dc9a582fab2d500d1f9f8b183c135
white-jang <galaxy__12@naver.com>
2021-02-22 00:29:44 +0900

    ~3chap

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="sabgiopsal" />
+      <Food fav="ramen" />
     </div>
   );
 }
